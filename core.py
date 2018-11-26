a54ecf5e7e424b764ef1cf9d6a4d7ca2c6fc9ef7
Ange <s4nixd@gmail.com>
2018-11-26 14:49:07 +0100

    Update the core.py by adding a pause sequence and make the render simple than before

--- core.py
+++ core.py
@@ -1,19 +1,22 @@
 import os
 import sys
 
-print "PyUp is checking if pip is installed on this machine......"
+print "> Starting....\n"
+print "> PyUp is checking if pip is installed......\n"
 
 # Here the code to do after for cheking if yes or no the pip is installed
 try:
     import pip
-    print "Pip is present."
-    print "-------------------------"
-    print "PyUp is installing pip-review ....."
+    print "> Pip is present."
+    print "----------------------------------------------------------------------------\n"
+    print "> PyUp is installing pip-review .....\n"
     os.system("pip install pip-review")
-    os.system("pip review")
     os.system("pip-review --auto")
+    print "----------------------------------------------------------------------------\n"
+    print "> PyUp has updated all your python packages."
 except ImportError:
-    print "Pip is not present."
-    print "-------------------------"
-    print "Please use this link to install PIP FIRST: https://github.com/BurntSushi/nfldb/wiki/Python-&-pip-Windows-installation"
+    print "> Pip is not present."
+    print "----------------------------------------------------------------------------\n"
+    print "> Please use this link to install PIP FIRST: https://github.com/BurntSushi/nfldb/wiki/Python-&-pip-Windows-installation"
     sys.exit()
+stop_theclosingcmd = raw_input(".")
