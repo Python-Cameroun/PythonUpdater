579306f13c7afc1b2d232ce8f55429dff2143061
Ange <s4nixd@gmail.com>
2018-11-27 10:19:32 +0100

    Adding a Try-Catch for pip_review and adding the web browser to open the link to install pip if there is no pip detected

--- core.py
+++ core.py
@@ -3,20 +3,39 @@ import sys
 
 print "> Starting....\n"
 print "> PyUp is checking if pip is installed......\n"
-
+separator = "----------------------------------------------------------------------------\n"
 # Here the code to do after for cheking if yes or no the pip is installed
 try:
     import pip
-    print "> Pip is present."
-    print "----------------------------------------------------------------------------\n"
-    print "> PyUp is installing pip-review .....\n"
-    os.system("pip install pip-review")
+    print "> PyUp: Pip is present.\n"
+    print separator
+    print "> PyUp: checking for pip-review .....\n"
+    try:
+        import pip_review
+        print "> PyUp: pip-review is present.\n"
+    except:
+        print "> PyUp: installing pip-review .....\n"
+        os.system("pip install pip-review")
     os.system("pip-review --auto")
-    print "----------------------------------------------------------------------------\n"
-    print "> PyUp has updated all your python packages."
+    print separator
+    print "> PyUp: All your python packages updated successfully.\n"
+    print separator
 except ImportError:
     print "> Pip is not present."
-    print "----------------------------------------------------------------------------\n"
-    print "> Please use this link to install PIP FIRST: https://github.com/BurntSushi/nfldb/wiki/Python-&-pip-Windows-installation"
+    print separator
+    installPIP = raw_input("> PyUp: Do you want to install it using your browser? (Y/N): ")
+    print separator
+    if installPIP.lower() == "y":
+        print "> PyUp: Opening in the browser....\n"
+        try:
+            import webbrowser
+        except:
+            print "> PyUp: You don't have webbrowser installed\n"
+            print separator
+            os.system("pip install webbrowser")
+        webbrowser.open('https://github.com/BurntSushi/nfldb/wiki/Python-&-pip-Windows-installation')
+        print separator
+    else:
+        print "> PyUp: Please use this link to install PIP FIRST, otherwise PyUp is not able to work: https://github.com/BurntSushi/nfldb/wiki/Python-&-pip-Windows-installation"
     sys.exit()
 stop_theclosingcmd = raw_input(".")
