6a391f773dbfebf483c543d729b05bd211f72076
Sanix Darker <saadjioa@gmail.com>
2019-01-07 16:41:26 +0100

    little updates

--- core.py
+++ core.py
@@ -25,10 +25,26 @@ try:
 except ImportError:
     print "> Pip is not present."
     print separator
-    installPIP = raw_input("> PyUp: Do you want to install it using your browser? (Y/N): ")
+    installPIP = raw_input("> PyUp: Do you want to install it ? (Y/N): ")
     print separator
     if installPIP.lower() == "y":
-        print "> PyUp: Opening in the browser....\n"
+        print "> PyUp: installing pip....\n"
+        os.system("python get-pip.py")
+
+        print "> PyUp: checking for pip-review .....\n"
+        try:
+            # Checking for pip-review, if not installed then pyUp will install it
+            import pip_review
+            print "> PyUp: pip-review is present.\n"
+        except:
+            print "> PyUp: installing pip-review .....\n"
+            os.system("pip install pip-review")
+        os.system("pip-review --auto")
+        print separator
+        print "> PyUp: All your python packages updated successfully.\n"
+        print separator
+
+    else:
         try:
             # Checking for webbrowser, if not installed then pyUp will install it
             import webbrowser
@@ -38,7 +54,6 @@ except ImportError:
             os.system("pip install webbrowser")
         webbrowser.open('https://github.com/BurntSushi/nfldb/wiki/Python-&-pip-Windows-installation')
         print separator
-    else:
         print "> PyUp: Please use this link to install PIP FIRST, otherwise PyUp is not able to work: https://github.com/BurntSushi/nfldb/wiki/Python-&-pip-Windows-installation"
     sys.exit()
 os.system("pause")
